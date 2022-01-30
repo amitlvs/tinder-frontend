49fbd314239d0d16ad7a49366a510c8027cc63a6
amitlvs <amit007.mail2@gmail.com>
2022-01-30 20:59:48 +0530

    tinder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,31 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { NavbarComponent } from './navbar/navbar.component';
+import { ImageCardComponent } from './image-card/image-card.component';
+import { FooterComponent } from './footer/footer.component';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MDBBootstrapModulesPro, MDBSpinningPreloader } from 'ng-uikit-pro-standard';
+import {MatCardModule} from '@angular/material/card';
+// import { SwipeCardsModule } from 'ng2-swipe-cards';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ImageCardComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatIconModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCardModule,
+    MDBBootstrapModulesPro.forRoot(),
+    // SwipeCardsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
